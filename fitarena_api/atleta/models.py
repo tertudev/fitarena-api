47501e9d3b196ae8f0576895d01cf191f3fbe813
tertudev <wendelgomesdh7@gmail.com>
2026-03-25 10:30:30 -0400

    feat(atleta): add relationships and foreign keys for Categoria and CentroTreinamento models

--- fitarena_api/atleta/models.py
+++ fitarena_api/atleta/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
+from fitarena_api.categorias.models import CategoriaModel
+from fitarena_api.centro_treinamento.models import CentroTreinamentoModel
 from fitarena_api.contrib.models import BaseModel
 
 class AtletaModel(BaseModel):
@@ -14,3 +16,7 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
+    categoria_id = Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates='atleta')
+    centro_treinamento_id = Mapped[int] = mapped_column(ForeignKey('centros_treinamento.pk_id'))
