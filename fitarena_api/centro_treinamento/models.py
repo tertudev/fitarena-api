502cae31866189ecf1602b023039c794580d6e59
tertudev <wendelgomesdh7@gmail.com>
2026-03-25 10:31:31 -0400

    feat(centro_treinamento): add CentroTreinamentoModel and CentroTreinamento schema for training center management

---
+++ fitarena_api/centro_treinamento/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String
+from fitarena_api.atleta.models import AtletaModel
+from fitarena_api.contrib.models import BaseModel
+
+class CentroTreinamentoModel(BaseModel):
+    __tablename__ = 'centros_treinamento'
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(20), nullable=False)
+    endereco: Mapped[str] = mapped_column(String(60), nullable=False)
+    proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
+    atleta: Mapped['AtletaModel'] = relationship(back_populates='centro_treinamento')
