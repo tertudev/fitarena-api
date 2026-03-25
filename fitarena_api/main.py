cab81905dffc40fd0d4a536296dd9ba95a3b3d8f
tertudev <wendelgomesdh7@gmail.com>
2026-03-25 09:29:39 -0400

    refactor(main): remove unnecessary main block for uvicorn server

--- fitarena_api/main.py
+++ fitarena_api/main.py
@@ -1,8 +1,3 @@
 from fastapi import FastAPI
 
 app = FastAPI(title='FitArenaApi')
-
-if __name__ == '__main__':
-    import uvicorn
-
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
