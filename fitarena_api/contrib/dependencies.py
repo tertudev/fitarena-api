43a9dc43f824931b0017b2bf9012cba0a36fb000
tertudev <wendelgomesdh7@gmail.com>
2026-03-29 18:18:23 -0400

    feat(fitarena_api/contrib/dependencies): add database dependency for async session management

---
+++ fitarena_api/contrib/dependencies.py
@@ -0,0 +1,6 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from fitarena_api.configs.database import get_session
+
+DatabaseDependency = Annotated[AsyncSession, Depends(get_session)]
